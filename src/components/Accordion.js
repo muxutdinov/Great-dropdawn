9a9e947dc1d3f5043b4f2129ad526f7525adeece
Jaxongir <jmuxutdinov@mail.ru>
2021-10-28 09:10:08 +0500

    full completed mini project

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,13 +1,64 @@
-import React from "react";
+import React, { useState } from "react";
 import { Data } from "./Data";
 import styled from "styled-components";
 import { IconContext } from "react-icons";
-// import { FiPlus, FiMinus } from "react-icons/fi";
+import { FiPlus, FiMinus } from "react-icons/fi";
 
-const AccordionSection = styled.div``;
-const Container = styled.div``;
+const AccordionSection = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+  position: relative;
+  background: #fff;
+`;
+const Container = styled.div`
+  position: absolute;
+  top: 30%;
+  box-shadow: 2px 10px 35px 1px rgba(153, 153, 153, 0.3);
+`;
+const Wrap = styled.div`
+  background: #272727;
+  color: #fff;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  text-align: center;
+  cursor: pointer;
 
+  h1 {
+    padding: 2rem;
+    font-size: 2rem;
+  }
+  span {
+    margin-right: 1.5rem;
+  }
+`;
+const Dropdawn = styled.div`
+  background: #1c1c1c;
+  color: #00ffb9;
+  width: 100%;
+  height: 100px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  border-bottom: 1px solid #00ffb9;
+  border-top: 1px solid #00ffb9;
+  p {
+    font-size: 2rem;
+  }
+`;
 const Accordion = () => {
+  const [clicked, setClicked] = useState(false);
+  const toggle = (index) => {
+    if (clicked === index) {
+      return setClicked(null);
+    }
+    setClicked(index);
+  };
   return (
     <IconContext.Provider value={{ color: "#00FFB9", size: "25px" }}>
       <AccordionSection>
@@ -15,14 +66,20 @@ const Accordion = () => {
           {Data.map((value, index) => {
             return (
               <>
-                <h1>{value.question}</h1>
-                <p>{value.answer}</p>
+                <Wrap onClick={() => toggle(index)} key={index}>
+                  <h1>{value.question}</h1>
+                  <span>{clicked === index ? <FiMinus /> : <FiPlus />}</span>
+                </Wrap>
+                {clicked === index ? (
+                  <Dropdawn>
+                    <p>{value.answer}</p>
+                  </Dropdawn>
+                ) : null}
               </>
             );
           })}
         </Container>
       </AccordionSection>
-      ;
     </IconContext.Provider>
   );
 };
--- src/components/Data.js
+++ src/components/Data.js
@@ -1,14 +1,14 @@
 export const Data = [
     {
-        question:'What is your name?',
+        question:'What is your first name?',
         answer:"Jaxongir"
     },
     {
-        question:'What is your name?',
-        answer:"Jaxongir"
+        question:'What is your last name?',
+        answer:"Muxutdinov"
     },
     {
-        question:'What is your name?',
-        answer:"Jaxongir"
+        question:'What is your full name?',
+        answer:"Jaxongir Muxutdinov"
     },
 ]
